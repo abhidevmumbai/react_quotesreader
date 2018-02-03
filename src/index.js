35f2ccd933536f2a981e6e4eb4c7ba1a1d1a0273
abhishek <abhidevmumbai@gmail.com>
2018-02-03 15:15:11 +0530

    Hooked up random quotes api

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import './index.css';
 
 class Quote extends React.Component {
     render() {
         return ( 
-            <div>{ this.props.text }</div>
+            <div className="quoteBox">
+                <div className="quote">{ this.props.quote }</div>
+                <div className="author">- { this.props.author }</div>
+            </div>
+        );
+    }
+}
+
+class Btn extends React.Component{
+    render() {
+        return (
+            <button className="getBtn" onClick={() => this.props.onClick()}>New quote</button>
         );
     }
 }
@@ -14,12 +26,44 @@ class Reader extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            text: 'This is a new quote!!!!'
-        };
+            quote: 'Random quote...',
+            author: ''
+        }
     }
+
+    componentDidMount() {
+        this.handleClick();
+    }
+
+    handleClick() {
+        axios.get(
+            'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
+            {
+                headers: {
+                    "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
+                    Accept: "application/json",
+                    "Content-Type": "application/x-www-form-urlencoded"
+                }
+        })
+        .then(res => {
+            if (typeof res.data === 'string') {
+                res.data = JSON.parse(res.data); 
+            }
+            let quote = res.data.quote;
+            let author = res.data.author;
+            this.setState({
+                quote: quote,
+                author: author
+            });
+        });
+    }
+
     render() {
-        return ( <
-            Quote text = { this.state.text } > < /Quote>
+        return (
+            <div className="container">
+                <Quote quote={this.state.quote} author={this.state.author}></Quote>
+                <Btn onClick={() => this.handleClick()}></Btn>
+            </div>
         );
     }
 }
